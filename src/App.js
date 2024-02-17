c23c7750fe9b7ef7171cbf02af109f47628558c0
Srikant Sahu <srikantsahu114@gmail.com>
2024-02-17 19:50:29 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -13,9 +13,11 @@ import {
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Blog from "./pages/Blog/Blog";
+import Home from "./pages/Home/Home";
+import Project from "./pages/Project/Project"
 
-const Home = React.lazy(() => import("./pages/Home/Home"));
-const Project = React.lazy(() => import("./pages/Project/Project"));
+// const Home = React.lazy(() => import("./pages/Home/Home"));
+// const Project = React.lazy(() => import("./pages/Project/Project"));
 
 function App() {
   const location = useLocation();
